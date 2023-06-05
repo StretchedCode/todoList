8d435c234f1b2cc485bf41c09f756788b58049da
Ghufran <ghufranshahid101@gmail.com>
2023-06-05 02:10:11 -0400

    Added functionality to create new tasks, also linked projects with related tasks, alongside loading them within DOMController

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import DOMcontroller from './functions/DOMcontroller';
 import Project from './functions/projects';
+import Todo from './functions/todo';
 import './stylesheets/stylesheet.css';
 
 const UIController = new DOMcontroller();
@@ -11,6 +12,24 @@ const makeProject = UIController.getProjectBtn();
 makeProject.addEventListener('click', () => {
   const projName = prompt('Please enter the name of your project.');
   const proj = new Project(projName);
+  UIController.setfocusProject(proj);
   projects.push(proj);
-  UIController.createProjectDOM(projName);
+  loadProjects();
 });
+const makeTask = UIController.getTaskBtn();
+makeTask.addEventListener('click', () => {
+  const taskName = prompt('Please enter the name of your task');
+  const task = new Todo(taskName);
+  UIController.getfocusProject().addTask(task);
+  UIController.displayProject();
+});
+
+function loadProjects() {
+  projects.forEach((project) => {
+    if (project.getLoaded() === false) {
+      UIController.createProjectDOM(project);
+    } else {
+      project.setLoaded();
+    }
+  });
+}
