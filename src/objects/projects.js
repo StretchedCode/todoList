d9917ebc95ecbe42aa50cfa520070a1cbd4cbb9e
Ghufran <ghufranshahid101@gmail.com>
2023-06-04 20:20:04 -0400

    Added file for creation of project objects

---
+++ src/objects/projects.js
@@ -0,0 +1,20 @@
+// Module that creates project objects through classes and exports it to index.js
+
+class project {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+}
