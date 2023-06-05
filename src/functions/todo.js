8f000d3c363d7fffa9bc80dc5c171012b2c03ecc
Ghufran <ghufranshahid101@gmail.com>
2023-06-05 02:11:06 -0400

    Added task Class

---
+++ src/functions/todo.js
@@ -0,0 +1,13 @@
+// Module that creates the todo objects
+
+class Todo {
+  constructor(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+export default Todo;
