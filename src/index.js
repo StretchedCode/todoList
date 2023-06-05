05364fab376304af59021598af95b0af466b904d
Ghufran <ghufranshahid101@gmail.com>
2023-06-04 22:37:37 -0400

    Added eventlistener to createProjectBtn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import DOMcontroller from './functions/DOMcontroller';
+import Project from './functions/projects';
 import './stylesheets/stylesheet.css';
 
 const UIController = new DOMcontroller();
+const projects = [];
 
 UIController.defaultLoad();
+
+const makeProject = UIController.getProjectBtn();
+makeProject.addEventListener('click', () => {
+  const projName = prompt('Please enter the name of your project.');
+  const proj = new Project(projName);
+  projects.push(proj);
+  UIController.createProjectDOM(projName);
+});
